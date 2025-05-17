4c9378b77a8c68c572262e469a42fbf0c81d2c8a
@UhLukas <119970521+UhLukas@users.noreply.github.com>
2025-05-17 22:40:33 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,7 @@ const filesToCache = [
   '/',
   '/index.html',
   '/manifest.json',
+  '/style.css',
   '/icons/icon-192.png',
   '/icons/icon-512.png'
 ];
@@ -13,8 +14,24 @@ self.addEventListener('install', event => {
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      )
+    )
   );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      caches.match('/index.html').then(response => response || fetch(event.request))
+    );
+  } else {
+    event.respondWith(
+      caches.match(event.request).then(response => response || fetch(event.request))
+    );
+  }
 });
